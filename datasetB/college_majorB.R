a44c722813ac14d41b2c9f9e77db701bdce2638d
Rakkia <xiaocl@uw.edu>
2018-03-05 17:27:40 -0800

    a

--- datasetB/college_majorB.R
+++
@@ -1,8 +0,0 @@
-library("dplyr")
-majorB <- read.csv("datasetB/majorB.csv",header=TRUE,stringsAsFactors=FALSE)
-majordata <- read.csv("csv_pus/majors-list.csv",header=TRUE,stringsAsFactors=FALSE)
-
-#merge datasets
-datasetB <- merge(majorB, majordata,by = "FOD1P")
-#merged data with major
-write.csv(datasetB, "datasetB.csv", row.names=FALSE)
