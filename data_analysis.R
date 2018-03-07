0ba17ffd31bb3e746b3df6dfe08e105eb9046dde
Harry Cho <hcho93@uw.edu>
2018-03-06 23:12:23 -0800

    combined data for shiny

--- data_analysis.R
+++ data_analysis.R
@@ -9,36 +9,35 @@ grad_degree <- fread(file = "data/grad_degree.csv", stringsAsFactors = FALSE, he
 recent_grads <- fread(file = "data/recent_grads.csv", stringsAsFactors = FALSE, header = TRUE)
 overall <- fread(file = "data/overall.csv", stringsAsFactors = FALSE, header = TRUE)
 
-# Select the 4 columns to be shown ("Major", "Major Category", "Median", "Unemployment Rate")
-# and fix the names by adding the given suffix
-selectFix <- function(df, suffix) {
-  fixed <- select(df, "Major", "Major_Category", "median_earnings", "unemployment_rate")
-  colnames(fixed)[c(3,4)] <- c(paste0("Median_", suffix), paste0("Unemployment_rate_", suffix))
-  return(fixed)
-}
 
 # 2012 vs 2016 Analysis
-recent_grads_comparison <- merge(selectFix(recent_grads_2010_2012, "2012"), selectFix(recent_grads, "2016"), 
-                                 by = c("Major", "Major_Category")) %>% 
-                           mutate(Median_difference = Median_2016 - Median_2012)
+# Compares the median earnings for bachelor students under the age of 28 in 2012 vs 2016
+recent_grads_comparison <- merge(recent_grads_2010_2012, recent_grads, by = c("Major", "Major_Category"), suffixes = c("_2012", "_2016")) %>% 
+                           mutate(Median_difference = median_earnings_2016 - median_earnings_2012) %>% 
+                           select(Major, Major_Category, median_earnings_2012, median_earnings_2016, Median_difference)
 
-# recent grads vs all (only bachelor) Earning Growth over Time Analysis
-earning_growth_comparison <- merge(selectFix(recent_grads, "young"), selectFix(all, "all"), 
-                                   by = c("Major", "Major_Category")) %>% 
-                             mutate(Median_difference = Median_all - Median_young)
+# Earnings Growth over Time after Graduation Analysis
+# Compares the median earnings for recent bachelor graduates (under the age of 28) vs people of all ages with only the bachelor's degree
+earning_growth_comparison <- merge(recent_grads, all, by = c("Major", "Major_Category"), suffixes = c("_young", "_all")) %>% 
+                             mutate(Median_difference = median_earnings_all - median_earnings_young) %>% 
+                             select(Major, Major_Category, Median_difference)
 
-# Grad school premium Analysis
-grad_comparison <- merge(selectFix(recent_grads, "bachelor"), selectFix(grad_degree, "graduate"), 
-                         by = c("Major", "Major_Category")) %>% 
-                   mutate(Median_difference = Median_graduate - Median_bachelor)
+# Grad School Premium Analysis
+# Analyze the extent to which going to graduate school increases earnings for each major
+grad_comparison <- merge(recent_grads, grad_degree, by = c("Major", "Major_Category"), suffixes = c("_bachelor", "_graduate")) %>% 
+                   mutate(Median_difference_gc = median_earnings_graduate - median_earnings_bachelor) %>% 
+                   select(Major, Major_Category, Median_difference_gc)
 
-# Male vs Female Analysis (Median Earnings vs. Women's share of majors)
+# Relationship between Median Earnings and Women's Share of Students in Each Major
+# Analyzes whether there is a relationship between the two factors
 earnings_female <- merge(overall, female, by = c("Major", "Major_Category"), suffixes = c("_overall", "_female")) %>% 
                    mutate(female_share = total_majors_female / total_majors_overall) %>%                  
                    select(Major, Major_Category, median_earnings_overall, female_share)
-                   
 
-
+#
+combined <- merge(recent_grads_comparison, earning_growth_comparison, by = c("Major", "Major_Category"), suffixes = c("_rgc", "_egc")) %>% 
+            merge(grad_comparison, by = c("Major", "Major_Category"), suffixes = c("", "_gc")) %>% 
+            merge(earnings_female, by = c("Major", "Major_Category"), suffixes = c("", "_ef"))
 
 
 
